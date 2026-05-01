139f770e398cc50be35bfb685900d74375f2f273
Arijeet-Kumar-Das <dasarijeetkumar@gmail.com>
2026-05-01 13:17:06 +0530

    feat: deployment setting updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,9 @@ import chatRoutes from "./routes/chatRoutes.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: "*",
+}));
 app.use(express.json());
 
 app.use("/api/upload", uploadRoutes);
